418ac75416fd27c3a2b42a01f5d11527450f88c2
Abimbola Ikuerowo <ikuerowo13@gmail.com>
2023-12-22 10:42:49 +0100

    New site

---
+++ js/script.js
@@ -0,0 +1,108 @@
+// PAGE SCROLLING TOP BUTTON
+let scrollingUp = () => {
+    let scrollTop = document.getElementById("myBtn");
+    let scl = document.documentElement.scrollTop;
+    let sclLen = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((scl * 100) / sclLen);
+    if (scl>100) {
+      scrollTop.style.display = "block";
+    }
+    else {
+      scrollTop.style.display = "none";
+    }
+    scrollTop.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+  }
+  window.onscroll = scrollingUp;
+// END PAGE SCROLLING TOP BUTTON
+
+// STICKY HEADER ONSCROLL TOP
+const body = document.body;
+    let lastScroll = 0;
+    window.addEventListener('scroll', () => {
+        const currentScroll = window.pageYOffset;
+        if (currentScroll <= 0) {
+            body.classList.remove("scroll-up");
+        }
+        if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+            body.classList.remove("scroll-up");
+            body.classList.add("scroll-down");
+        }
+        if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+            body.classList.remove("scroll-down");
+            body.classList.add("scroll-up");
+        }
+
+        lastScroll = currentScroll;
+});
+// END STICKY HEADER ONSCROLL TOP
+
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll(".prd")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+
+const showHideIcons = () => {
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; 
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; 
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); 
+    });
+});
+
+const autoSlide = () => {
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+
+    positionDiff = Math.abs(positionDiff); 
+    let firstImgWidth = firstImg.clientWidth + 14;
+    let valDifference = firstImgWidth - positionDiff;
+
+    if(carousel.scrollLeft > prevScrollLeft) { 
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
